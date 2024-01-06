4887c1db6dfc5d556ec861514ea9792ccc5b13c1
Pozsar Zsolt <pozsarzs@karak.lan>
2024-01-06 21:02:23 +0100

    240106

--- source/protcom/protcom.pas
+++ source/protcom/protcom.pas
@@ -21,20 +21,71 @@
 {$ASMMODE INTEL}
 unit protcom;
 interface
-
+uses
+  go32,
+  sysutils;
+var
+  newvec: tseginfo; // new interrupt vector
+  oldvec: tseginfo; // old interrupt vector
+  orgds: word; external name '___v2prt0_ds_alias';
+  rda: boolean = false; // received data available
+  ba: word; // base address of the device
+  irq: byte; // IRQ line of the device
+  error: boolean = false; // collected error flag
+  
+const
+  // default base address and IRQ
+  DEF_BA: array[1..4] of word = ($03f8, $02f8, $02e8, $03e8);
+  DEF_IRQ: array[1..4] of byte = (4, 3, 4, 3);
+  // offset of register addresses from the base address
+  THR = $00; // Transmit Holding Register
+  RBR = $00; // Receive Buffer Register
+  DLL = $00; // Divisor Latch Low byte
+  DLH = $01; // Divisor Latch High byte
+  IER = $01; // Interrupt Enable Register
+  IIR = $02; // Interrupt Identification Register
+  LCR = $03; // Line Control Register
+  MCR = $04; // Modem Control Register
+  LSR = $05; // Line Status Register
+  MSR = $06; // Modem Status Register
+  SCR = $07; // Scratch Register
+  
 function canread(timeout: integer): boolean;
 function canwrite(timeout: integer): boolean;
 function getportaddr: word; 
 function recvbyte(timeout: integer): byte;
-function recvstring(timeout: integer): ansistring;
+function recvstring(timeout: integer): string;
 procedure config(baud, bits: integer; parity: char; stop: integer);
 procedure connect(comport: string);
 procedure sendbyte(data: byte);
-procedure sendstring(data: ansistring);
-procedure setsizerecvbuffer(size: integer);
+procedure sendstring(data: string);
 
 implementation
 
+procedure recvirq; assembler;
+asm
+end;
+
+procedure recvirqend; assembler;
+asm
+end;
+
+function  getchar: char; assembler;
+asm
+end;
+
+procedure putchar(c: char); assembler;
+asm
+end;
+
+procedure setspeed(speed: word); assembler;
+asm
+end;
+
+procedure init(defaults: byte); assembler;
+asm
+end;
+
 function canread(timeout: integer): boolean;
 begin
 end;
@@ -51,7 +102,7 @@ function recvbyte(timeout: integer): byte;
 begin
 end;
 
-function recvstring(timeout: integer): ansistring;
+function recvstring(timeout: integer): string;
 begin
 end;
 
@@ -60,19 +111,36 @@ begin
 end;
 
 procedure connect(comport: string);
+var
+  i: byte;
+  s: string = '';
 begin
+  error := false;
+  if length(comport) >= 4 then
+  begin
+    for i := 1 to 3 do s := s + comport[i];
+    if comparestr(uppercase(s),'COM') = 0 then
+    begin
+      i := strtointdef(comport[4], 0);
+      if (i > 0) and (i < 5) then
+      begin
+        ba := DEF_BA[i];
+        irq := DEF_IRQ[i];
+      end else error := true;
+    end;
+  end;
 end;
 
 procedure sendbyte(data: byte);
 begin
-end;
+  putchar(chr(data));
+end; 
 
-procedure sendstring(data: ansistring);
+procedure sendstring(data: string);
+var
+  b: byte;
 begin
-end;
-
-procedure setsizerecvbuffer(size: integer);
-begin
+  for b := 1 to length(data) do putchar(data[b]);
 end;
 
 end.
