b12b3764294f14468e325ba52f4229383adf345a
Pozsar Zsolt <pozsarzs@karak.lan>
2024-10-13 10:14:48 +0200

    241013

--- source/builddos.bat
+++ source/builddos.bat
@@ -1,5 +1,8 @@
 @echo off
-rem Build program for DOS (32 bit protected mode)
+rem ModShell 0.1 * Command-driven scriptable Modbus utility
+rem Copyright (C) 2023-2024 Pozsar Zsolt pozsarzs@gmail.com
+rem builddos.bat
+rem Build program for DOS in 32 bit protected mode
 
 cd modshell
 make -f Makefile.dos
--- source/buildw32.bat
+++ source/buildw32.bat
@@ -1,8 +1,34 @@
 @echo off
-rem Build programs for 32 bit Windows
+rem ModShell 0.1 * Command-driven scriptable Modbus utility
+rem Copyright (C) 2023-2024 Pozsar Zsolt pozsarzs@gmail.com
+rem buildwin32.bat
+rem Build program for 32 bit Windows
 
+set buildcui=1
+set buildgui=1
+
+:loop
+if "%1"=="" goto :done
+if "%1"=="/?" goto :help
+if "%1"=="/nocui" set buildcui=0
+if "%1"=="/nogui" set buildgui=0
+shift
+goto :loop
+:done
+
+if %buildcui%==0 goto :gui
 cd modshell
 make -f Makefile.w32
-cd ..\xmodshell
+cd ..
+:gui
+if %buildgui%==0 goto :end
+cd xmodshell
 make -f Makefile.w32
 cd ..
+goto end
+
+:help
+echo Usage: "%0 [/?] [nocui] [nogui]"
+goto end
+
+:end
