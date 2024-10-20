065a6c0871250ea3011525ad93f8fa47beb1105b
Pozsar Zsolt <pozsarzs@karak.lan>
2024-10-20 09:38:52 +0200

    241020

--- source/_inc/parsing.pas
+++ source/_inc/parsing.pas
@@ -214,7 +214,7 @@ begin
         vars[0].vvalue := inttostr(exitcode);
         {$IFDEF X}
           Form1.ComboBox1.Enabled := true;
-          if f3active then Form3.Memo1.SetFocus else Form1.ComboBox1.SetFocus;
+          if f3active then Form3.Memo1.SetFocus; // else Form1.ComboBox1.SetFocus;
         {$ENDIF}
       end else {$IFNDEF X} writeln(ERR00); {$ELSE} Form1.Memo1.Lines.Add(ERR00); {$ENDIF}
     end;
